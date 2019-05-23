58899e25244ef46d9244432186a7b8ae1a8f3c4d
Trevor hoffman <trevorhoffman@Trevors-MacBook-Air.local>
2019-05-23 17:30:03 -0500

    signup page done

---
+++ src/components/Login/Login.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+// import { login } from "../../store";
+// import { connect } from "react-redux";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.login(this.state.username, this.state.password).catch(err => {
+      console.log(err);
+      alert("invalid username or password");
+    });
+    this.setState({
+      username: "",
+      password: ""
+    });
+  };
+
+  render() {
+    console.log(this.state.password);
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>username</label>
+        <input
+          name="username"
+          onChange={this.handleChange}
+          value={this.state.username}
+        />
+        <label>password</label>
+        <input
+          name="password"
+          onChange={this.handleChange}
+          value={this.state.password}
+        />
+        <button onClick={this.handleSubmit}>Submit</button>
+      </form>
+    );
+  }
+}
+// const mapStateToProps = reduxState => reduxState;
+
+// export default connect(
+//   mapStateToProps,
+//   { login }
+// )(Login);
+export default Login;
