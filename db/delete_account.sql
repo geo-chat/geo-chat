71e684a4ad0b4069ee9767d6e21d10dd339bb656
A R Bradley <you@example.com>
2019-05-28 09:11:46 -0500

    merge conflicts

--- db/delete_account.sql
+++ db/delete_account.sql
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-DELETE FROM chatuser WHERE id = $1;
-=======
 DELETE * FROM chatuser WHERE id = $1;
->>>>>>> master
--- db/signup.sql
+++ db/signup.sql
@@ -1,7 +1,3 @@
 INSERT INTO chatuser(username,password)
-<<<<<<< HEAD
 VALUES($1,$2);
-=======
-values($1,$2);
->>>>>>> master
 SELECT * FROM chatuser WHERE username = $1;
