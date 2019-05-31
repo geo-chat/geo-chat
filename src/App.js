7db76efa6f89abebf0494e80e76a40a83adc44ea
Carlos <carlos@Carloss-MacBook-Air.local>
2019-05-31 12:14:16 -0500

    routes for chatroom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from "react";
-import "./App.css";
-import { HashRouter } from "react-router-dom";
-import routes from "./routes";
-import { Provider } from "react-redux";
-import store from "./store";
-import Chatroom from "./components/Chatroom/Chatroom";
-import Login from "./components/Login/Login";
+import React from 'react';
+import './App.css';
+import { HashRouter } from 'react-router-dom';
+import routes from './routes';
+import { Provider } from 'react-redux';
+import store from './store';
+import Chatroom from './components/Chatroom/Chatroom';
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
 
 function App() {
-  return (
-    <Provider store={store}>
-      <HashRouter>
-        <Login />
-        <Chatroom />
-
-        {/* <div className="App">{routes}</div> */}
-      </HashRouter>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<HashRouter>{routes}</HashRouter>
+		</Provider>
+	);
 }
 
 export default App;
